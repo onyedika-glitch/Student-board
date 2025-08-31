169f2014c1d728b5a496263c19b820f94f28b0f9
Omogo Peter <omogopeter48@gmail.com>
2025-08-31 11:52:32 +0100

    Add new files

--- student-board/resources/views/layouts/app.blade.php
+++ student-board/resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ config('app.name', 'Student Board') }}</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body class="font-sans antialiased bg-gray-100">
     <div class="min-h-screen bg-gray-100">

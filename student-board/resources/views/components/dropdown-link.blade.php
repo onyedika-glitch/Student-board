ee7093823bb4b217dfc0cf6d6b54f0395200d691
Omogo Peter <omogopeter48@gmail.com>
2025-09-26 22:48:52 +0100

    Add new files

--- student-board/resources/views/components/dropdown-link.blade.php
+++ student-board/resources/views/components/dropdown-link.blade.php
@@ -6,6 +6,6 @@ $classes = ($active ?? false)
             : 'block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out';
 @endphp
 
-<a {{ $attributes->merge(['class' => $classes]) }}>
+<a href="{{ $href }}" {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
--- student-board/resources/views/layouts/nav.blade.php
+++ student-board/resources/views/layouts/nav.blade.php
@@ -57,7 +57,7 @@
                         </x-slot>
 
                         <x-slot name="content">
-                            <x-dropdown-link :href="route('profile.edit')">
+                            <x-dropdown-link :href="route('profile.show')">
                                 {{ __('Profile') }}
                             </x-dropdown-link>
 
@@ -132,7 +132,7 @@
                 </div>
 
                 <div class="mt-3 space-y-1">
-                    <x-responsive-nav-link :href="route('profile.edit')">
+                    <x-responsive-nav-link :href="route('profile.show')">
                         {{ __('Profile') }}
                     </x-responsive-nav-link>
 
